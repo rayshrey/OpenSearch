bd6a1195bc885a295d66b02274c011abe6e08290
rayshrey <121871912+rayshrey@users.noreply.github.com>
2025-05-12 15:18:26 +0530

    Add method for getting reference count of entry in FileCache  (#18259)
    Signed-off-by: Shreyansh Ray <rayshrey@amazon.com>

--- server/src/main/java/org/opensearch/index/store/remote/filecache/FileCache.java
+++ server/src/main/java/org/opensearch/index/store/remote/filecache/FileCache.java
@@ -126,6 +126,11 @@ public class FileCache implements RefCountedCache<Path, CachedIndexInput> {
     }
 
     @Override
+    public Integer getRef(Path key) {
+        return theCache.getRef(key);
+    }
+
+    @Override
     public long prune() {
         return theCache.prune();
     }
